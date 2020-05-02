03f909769ad7b57b405bbe7412cdffb9204f6f32
radical Ed <34996308+jrodbshaw@users.noreply.github.com>
2020-05-02 12:21:35 +1000

    update shop data/ cart utils to group items in cart reducer

--- src/components/collectionItem/CollectionItem.js
+++ src/components/collectionItem/CollectionItem.js
@@ -1,8 +1,13 @@
 import React from "react";
-
+import { connect } from "react-redux";
+// * redux
+import { addItem } from "../../redux/cart/cart.actions";
+// * styles and components
 import "./collection-item.styles.scss";
+import CustomButton from "../CustomButton/CustomButtom";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ background: `url(${imageUrl})` }} />
@@ -10,8 +15,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,12 +1,18 @@
 import CartActionTypes from "./cart.types";
 
-const initialState = { hidden: true };
+import { addItemToCart } from "./cart.utils";
 
-const cartReducer = (state = initialState, { type }) => {
+const initialState = { hidden: true, cartItems: [] };
+
+const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
-
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload),
+      };
     default:
       return state;
   }
